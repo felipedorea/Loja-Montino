b26bd2cd6e8b0649892b919bcd175f9f4528c23d
felipedorea <122095865+felipedorea@users.noreply.github.com>
2023-10-08 17:21:48 -0300

    .

--- script/script.js
+++ script/script.js
@@ -3,4 +3,12 @@ $(".mobile-btn").click(function(){
     $(".nav-items-mobile").css("display", "flex");   
 });
 
+$("#eletrico").hover(function(){
+    $("#eletrico-titulo").css("opacity", "1");
+    $("#eletrico-img").css("filter", "blur(3px)")
+}, function(){
+    $("#eletrico-titulo").css("opacity", "0")
+    $("#eletrico-img").css("filter", "blur()")
+})
+
     

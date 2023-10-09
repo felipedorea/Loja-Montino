2fa607c85cf3b14059011fae6b0db101c0c4ee13
felipedorea <122095865+felipedorea@users.noreply.github.com>
2023-10-09 11:56:11 -0300

    .

--- script/script.js
+++ script/script.js
@@ -11,4 +11,20 @@ $("#eletrico").hover(function(){
     $("#eletrico-img").css("filter", "blur()")
 })
 
+$("#hidraulico").hover(function(){
+    $("#hidraulico-titulo").css("opacity", "1");
+    $("#hidraulico-img").css("filter", "blur(3px)")
+}, function(){
+    $("#hidraulico-titulo").css("opacity", "0")
+    $("#hidraulico-img").css("filter", "blur()")
+})
+
+$("#construcao").hover(function(){
+    $("#construcao-titulo").css("opacity", "1");
+    $("#construcao-img").css("filter", "blur(3px)")
+}, function(){
+    $("#construcao-titulo").css("opacity", "0")
+    $("#construcao-img").css("filter", "blur()")
+})
+
     

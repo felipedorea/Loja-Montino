301457b2d8970cc5e377e79003cb39f50d09bd40
felipedorea <122095865+felipedorea@users.noreply.github.com>
2023-10-26 20:15:32 -0300

    .

--- script/accordion.js
+++ script/accordion.js
@@ -9,3 +9,27 @@ $("#title2").on("click", function(){
     $("#title2 > i").toggleClass("chevron")
     $("#content2").slideToggle("slow")
 })
+
+$("#title3").on("click", function(){
+    $("#title3").toggleClass("active");
+    $("#title3 > i").toggleClass("chevron")
+    $("#content3").slideToggle("slow")
+})
+
+$("#title4").on("click", function(){
+    $("#title4").toggleClass("active");
+    $("#title4 > i").toggleClass("chevron")
+    $("#content4").slideToggle("slow")
+})
+
+$("#title5").on("click", function(){
+    $("#title5").toggleClass("active");
+    $("#title5 > i").toggleClass("chevron")
+    $("#content5").slideToggle("slow")
+})
+
+$("#title6").on("click", function(){
+    $("#title6").toggleClass("active");
+    $("#title6 > i").toggleClass("chevron")
+    $("#content6").slideToggle("slow")
+})

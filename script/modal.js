269f2cb42e73040f91f65f538707acca7f0d97c8
felipedorea <122095865+felipedorea@users.noreply.github.com>
2023-11-01 10:48:48 -0300

    .

---
+++ script/modal.js
@@ -0,0 +1,8 @@
+$(".ficha-tecnica-btn").on('click', function(){
+    $(".modal-detalhes").css("right", "0");
+});
+
+$(".fechar-btn").on('click', function(){
+    $(".modal-detalhes").css("right", "-100%");
+});
+
